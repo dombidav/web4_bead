087c8b7b9f0a466e6064906c5bf9c9c099cb72c9
Dombi Tibor <dombidav@gmail.com>
2021-01-24 22:41:43 +0100

    Testing router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const db = require('../db').default
 const router = express.Router();
 
-router.get('/', (req, res, next) =>{
-    res.json({
-        Hello: "World"
-    })
+router.get('/', async (req, res, next) =>{
+    try {
+        let results = await db.all('users')
+        res.json(results)
+    } catch (error) {
+        res.sendStatus(500)
+        res.json(error)
+    }
+})
+
+router.get('/:id', async (req, res, next) =>{
+    try {
+        let results = await db.one('users', req.params.id)
+        res.json(results)
+    } catch (error) {
+        res.sendStatus(500)
+        res.json(error)
+    }
 })
 
 module.exports = router
